b941e4166e922473ebbabc7fa2d147b551dde342
xiangyu789 <2076063783@qq.com>
2026-04-21 20:30:26 +0800

    Add files via upload
    非负数各个位相加直到和小于10

--- main.c
+++ main.c
@@ -1,37 +1,18 @@
-#include<stdio.h>
-int lj(int n)
-{
-    if(n==1)
-    return 1;
-    else if (n>1)
+int addDigits(int num) {
+    int d=0;
+    while(1)
     {
-    return n+lj(n-1);
-    }
-}
-int san(int n)
-{
-    if(n==1)
-    return 1;
-    else{
-        return san(n-1)+lj(n-1);
-    }
-}
-int main()
-{
-    int n,i,arr[100];
-    scanf("%d\n",&n);
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&arr[i]);
-    }
-    for(i=0;i<n;i++)
-    {
-        if(n==1)
+        d+=num%10;
+        num/=10;
+        if(num<=0)
         {
-            printf("1");
+            if(d<10)
+            {
+                break;
+            }
+            num=d;
+            d=0;
         }
-        else
-        printf("%d\n",arr[i]+(arr[i]-1)+(arr[i]-2)+san(n));
     }
-    return 0;
+    return d;
 }
